230a2d216eaddcd9862a69f242dc506a770190dd
vladliena <sotnikovavladlena@gmail.com>
2022-12-21 17:43:35 +0200

    const arrayIndex added

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,10 @@
 function removeElement(array, item) {
     const finalArray = []
-    let arrayIndex = array.indexOf(item)
+    const arrayIndex = array.indexOf(item)
     if (arrayIndex !== -1) {
         array.splice(arrayIndex, 1)
-        Array.prototype.push.apply(finalArray,array)
     }
-    return finalArray
-    // вместо Array.prototype.push.apply(finalArray,array) можно написать return finalArray.concat(array)
+    return finalArray.concat(array)
 }
 
 console.log(removeElement([1, 2, 3, 4, 5, 6, 7,],5))
